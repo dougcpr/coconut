b691c5f79997bce156f1a4cd9c4e03fb48b09fe9
Douglas Cooper <doug.cpr@gmail.com>
2023-06-12 20:13:43 -0700

    added prisma client to add rows to postgres db

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import express, {Express, Request, Response} from "express";
+import { PrismaClient } from '@prisma/client'
 
 const dotenv = require('dotenv');
 
@@ -6,11 +7,27 @@ dotenv.config();
 
 const app: Express = express();
 const port: (string | undefined) = process.env.PORT;
+const prisma = new PrismaClient();
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Express + Typescript + Nodemon')
+    res.send('Express + Typescript + Nodemon + Prisma')
 });
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 })
+
+async function main() {
+    // write queries here
+}
+
+
+main()
+  .then(async () => {
+      await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+      console.error(e)
+      await prisma.$disconnect()
+      process.exit(1)
+  })
