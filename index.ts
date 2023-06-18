a083d48359e22a0404381c40045a9495ea283a1d
Douglas Cooper <doug.cpr@gmail.com>
2023-06-17 18:32:38 -0700

    removed trpc will place in client that consumes server

--- index.ts
+++ index.ts
@@ -1,8 +1,5 @@
 import express, {Express, Request, Response} from "express";
-import {PrismaClient} from '@prisma/client'
-import {inferAsyncReturnType, initTRPC} from '@trpc/server';
-import * as trpcExpress from '@trpc/server/adapters/express';
-import {z} from 'zod';
+import { PrismaClient } from '@prisma/client'
 
 const dotenv = require('dotenv');
 
@@ -16,46 +13,14 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Express + Typescript + Nodemon + Prisma')
 });
 
-// created for each request
-const createContext = ({
-                           req,
-                           res,
-                       }: trpcExpress.CreateExpressContextOptions) => ({}); // no context
-type Context = inferAsyncReturnType<typeof createContext>;
-const t = initTRPC.context<Context>().create();
-export const appRouter = t.router({
-    getUser: t.procedure
-        .input(z.string()).query(async (opts) => {
-            opts.input; // string
-            return prisma.user.findMany({
-                where: {
-                    email: opts.input
-                },
-                include: {
-                    posts: true,
-                    profile: true,
-                },
-            });
-        }),
-    createUser: t.procedure
-        .input(z.object({
-            name: z.string().min(3),
-            email: z.string().email(),
-        }))
-        .mutation(async (opts) => {
-            // use prisma to create user
-            return prisma.user.create({
-                data: {
-                    name: opts.input.name,
-                    email: opts.input.email
-                },
-            });
-        }),
-});
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+})
 
 async function main() {
     // write queries here
 }
+
 
 main()
     .then(async () => {
@@ -66,15 +31,3 @@ main()
         await prisma.$disconnect()
         process.exit(1)
     })
-
-app.use(
-    '/api/trpc',
-    trpcExpress.createExpressMiddleware({
-        router: appRouter,
-        createContext,
-    }),
-);
-
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
-})
